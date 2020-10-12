df2bf396c63233bc43e315eac142be1de33b5779
uaolmer <uaolmer2@gmail.com>
2020-10-12 22:43:04 +0300

    feat: create tasks, boards routes

--- src/resources/boards/board.router.js
+++ src/resources/boards/board.router.js
@@ -7,4 +7,24 @@ router.route('/').get(async (req, res) => {
   res.json(boards.map(Board.toResponse));
 });
 
+router.route('/:id').get(async (req, res) => {
+  const board = await boardsService.read(req.params.id);
+  res.sendStatus(200).send(Board.toResponse(board));
+});
+
+router.route('/').post(async (req, res) => {
+  const board = await Board.create(Board.fromRequest(req.body));
+  res.sendStatus(200).send(Board.toResponse(board));
+});
+
+router.route('/:id').put(async (req, res) => {
+  const board = await boardsService.update(Board.fromRequest(req.body));
+  res.sendStatus(200).send(Board.toResponse(board));
+});
+
+router.route('/:id').delete(async (req, res) => {
+  await boardsService.remove(req.params.id);
+  res.sendStatus(200).send('Item removed');
+});
+
 module.exports = router;
--- src/resources/tasks/task.router.js
+++ src/resources/tasks/task.router.js
@@ -7,4 +7,24 @@ router.route('/').get(async (req, res) => {
   res.json(tasks.map(Task.toResponse));
 });
 
+router.route('/:id').get(async (req, res) => {
+  const task = await tasksService.read(req.params.id);
+  res.sendStatus(200).send(Task.toResponse(task));
+});
+
+router.route('/').post(async (req, res) => {
+  const task = await Task.create(Task.fromRequest(req.body));
+  res.sendStatus(200).send(Task.toResponse(task));
+});
+
+router.route('/:id').put(async (req, res) => {
+  const task = await tasksService.update(Task.fromRequest(req.body));
+  res.sendStatus(200).send(Task.toResponse(task));
+});
+
+router.route('/:id').delete(async (req, res) => {
+  await tasksService.remove(req.params.id);
+  res.sendStatus(200).send('Item removed');
+});
+
 module.exports = router;
